00b7919e7b247dc1818462fcbd0768704b3da8e1
onkarsabale15 <onkarsabale15@gmail.com>
2023-03-01 21:10:48 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+const env = require('dotenv')
+env.config()
 const express = require('express');
 const app = express();
 const https = require('https')
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
     console.log("The Server Has Started\nListening on port :", PORT, `\nTo Access on local Machine ctrl + click on ==> http://localhost:${PORT}/`)
 });
 
 let lon = '73.75'
 let lat = '18.64'
-let api_key = '934ede4e3ee38b51eb16e280d1cc1224'
+let api_key = process.env.API_KEY
 let dta
 https.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,(resp)=>{
-    res.on(data)
+    resp.on('data', (data)=>{
+        dta = JSON.parse(data)
+    })
 })
 
 app.get('/', (req,res)=>{
