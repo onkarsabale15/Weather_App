8d792395ce1e0a95fc3729961ea0898c8930900b
onkarsabale15 <onkarsabale15@gmail.com>
2023-03-09 23:08:19 +0530

    build try 1

--- server.js
+++ server.js
@@ -10,20 +10,13 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log("The Server Has Started\nListening on port :", PORT, `\nTo Access on local Machine ctrl + click on ==> http://localhost:${PORT}/`)
 });
-
-
-
-
 app.get('/', (req, res) => {
     console.log("A New User Just Connected")
     res.sendFile(__dirname + '/index.html')
 });
-
 app.post('/', (req, res) => {
     const lat = req.body.latitude;
     const lon = req.body.longitude;
-    // console.log(`Received location: ${latitude}, ${longitude}`);
-    // res.sendStatus(200);
     let api_key = process.env.API_KEY
     let dta
     https.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`, (resp) => {
@@ -32,6 +25,4 @@ app.post('/', (req, res) => {
             res.send(data)
         })
     })
-    // console.log(dta)
-    // res.send(dta)
 });
